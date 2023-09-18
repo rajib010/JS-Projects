7e0820f065728d4cfac8c64ceb7d926b726f4425
Rajib Pokhrel <pokhrelrajib015@gmail.com>
2023-09-18 14:32:28 +0545

    project 3

---
+++ ImageSearchEngine/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const apiKey = "sZk11QMVoZ5_IveisrK8c5ECdDZFknoKZ310OqIlKTo";
+
+const searchForm = document.querySelector(".search-form");
+const searchFor = document.querySelector("#search-for");
+const searchBtn = document.querySelector("#search-btn");
+const searchResults = document.querySelector(".search-results");
+const showMoreBtn = document.querySelector("#show-moreBtn");
+const description = document.querySelector("#description");
+
+let query = "";
+let page = 1;
+
+async function getImages() {
+    // Clear the search results before displaying the new results.
+    searchResults.innerHTML = "";
+
+    query = searchFor.value;
+
+    if (!query) {
+        alert("Search Field Cannot be Empty.");
+    } else {
+        const url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&per_page=12&client_id=${apiKey}`;
+        const response = await fetch(url);
+        let data = await response.json();
+        description.style.display = 'block';
+
+
+        const results = data.results;
+        //check if there are 0 results
+        if (results != 0) {
+            if (page === 1) {
+            }
+
+            //render the each element of array into the html
+            results.map((result) => {
+                const image = document.createElement("img");
+                image.src = result.urls.small;
+                const imgLinks = document.createElement("a");
+                imgLinks.href = result.links.html;
+                imgLinks.target = "_blank";
+                imgLinks.appendChild(image);
+                searchResults.appendChild(imgLinks);
+            })
+            description.textContent = ` Showing available images for "${query}"`;
+            showMoreBtn.style.display = 'block';
+        } else {
+            description.textContent = ` OOPS!! No images available for"${query}". Try searching something differnt.`;
+        }
+    }
+}
+
+//to run the function after someone submits something in the search box...
+searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();//stop the page from reloading..
+    page = 1;
+    getImages();
+})
+
+showMoreBtn.addEventListener("click", () => {
+    page++;
+    getImages();
+})
